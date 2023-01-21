a08a8e4fef9719e0e5d62fcf326c6958392167aa
kseniya shukevich <kseniya.shukevich@gmail.com>
2023-01-21 08:52:38 +0100

    refactor: Static server

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 // import { mouse } from '@nut-tree/nut-js';
-import httpServer from './server/http_server/index';
+import httpServer from './server/httpServer/index';
+import wsServer from './server/webSocket/index';
 
 const HTTP_PORT = 8181;
-
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
+const WS_PORT = 8080;
 
 httpServer.listen(HTTP_PORT);
+wsServer.listen(WS_PORT);
